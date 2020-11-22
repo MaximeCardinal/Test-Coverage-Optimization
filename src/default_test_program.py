9236f68b07bcd5f2dd174760e0038a90f717e521
alexis franche <alexis.franche@mail.mcgill.ca>
2020-11-22 14:51:13 -0500

    operations

--- src/default_test_program.py
+++ src/default_test_program.py
@@ -1,4 +1,6 @@
+from math import sqrt
 import sys
+import math
 
 def test():
     if (len(sys.argv) != 3):
@@ -7,9 +9,30 @@ def test():
 
     number1 = float(sys.argv[1])
     number2 = float(sys.argv[2])
+    number3 = float(sys.argv[3])
+
+    biggest = 0
 
     if (number1 >= number2):
         biggest = number1 
+    elif number3 > number1 and number3 > number1:
+        myStr = "I AM THE" 
+        myStr = myStr + "BIGGEST"
+        for i in range(100000):
+            myStr = myStr + "i"
+    elif number3 < number1 and number3 < number1:
+        myStr = "I AM THE" 
+        myStr = myStr + "SMALLEST"
+        for i in range(100000):
+            myStr = myStr + "i"
+    elif number1 > number2 and number1 > number3:
+        someStr= "HUUUUUGE" + "CALCulations" + "HERE"
+        result = math.pi * math.ceil(897468156) + math.comb(50,5) + math.acos(0.5)
+        result = result + math.pi**2**2 - math.atan2(0.015, .99)
+        result = math.abs(result)
+        result = sqrt(result) / (result % 9999999)
+        for i in range(100000):
+            result = result + 1
     else:
         biggest = number2
         biggest = biggest + 1
